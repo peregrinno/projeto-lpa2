81175da310dc6902f456b8fe100c63df1e5f9c65
sgeizydev <sgeizianny@gmail.com>
2021-04-16 17:13:44 -0300

    Conecta o formsServico ao banco de dados;

--- Forms/FormServicos.Designer.cs
+++ Forms/FormServicos.Designer.cs
@@ -32,20 +32,17 @@ namespace ModernArt.Forms
             this.radioButton2 = new System.Windows.Forms.RadioButton();
             this.radioButton1 = new System.Windows.Forms.RadioButton();
             this.label6 = new System.Windows.Forms.Label();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.label5 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.valorBaseServico = new System.Windows.Forms.TextBox();
+            this.servicoNome = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.TabelaServico = new System.Windows.Forms.DataGridView();
+            this.AddServico = new System.Windows.Forms.Button();
+            this.alterarServico = new System.Windows.Forms.Button();
+            this.deletarServicos = new System.Windows.Forms.Button();
+            this.comboServicos = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.TabelaServico)).BeginInit();
             this.SuspendLayout();
             // 
             // radioButton2
@@ -80,148 +77,126 @@ namespace ModernArt.Forms
             this.label6.TabIndex = 29;
             this.label6.Text = "label6";
             // 
-            // textBox4
+            // valorBaseServico
             // 
-            this.textBox4.Location = new System.Drawing.Point(63, 160);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(27, 20);
-            this.textBox4.TabIndex = 28;
+            this.valorBaseServico.Location = new System.Drawing.Point(63, 108);
+            this.valorBaseServico.Name = "valorBaseServico";
+            this.valorBaseServico.Size = new System.Drawing.Size(224, 20);
+            this.valorBaseServico.TabIndex = 25;
             // 
-            // label5
+            // servicoNome
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(22, 163);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(35, 13);
-            this.label5.TabIndex = 27;
-            this.label5.Text = "label5";
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(63, 134);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(110, 20);
-            this.textBox3.TabIndex = 26;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(63, 108);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(224, 20);
-            this.textBox2.TabIndex = 25;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(63, 82);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(224, 20);
-            this.textBox1.TabIndex = 24;
+            this.servicoNome.Location = new System.Drawing.Point(63, 82);
+            this.servicoNome.Name = "servicoNome";
+            this.servicoNome.Size = new System.Drawing.Size(224, 20);
+            this.servicoNome.TabIndex = 24;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(333, 47);
+            this.label4.Location = new System.Drawing.Point(338, 53);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(70, 26);
+            this.label4.Size = new System.Drawing.Size(127, 26);
             this.label4.TabIndex = 23;
-            this.label4.Text = "label4";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(22, 137);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
-            this.label3.TabIndex = 22;
-            this.label3.Text = "label3";
+            this.label4.Text = "SERVIÇOS";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(22, 111);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.Size = new System.Drawing.Size(46, 13);
             this.label2.TabIndex = 21;
-            this.label2.Text = "label2";
+            this.label2.Text = "VALOR ";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(22, 85);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(39, 13);
             this.label1.TabIndex = 20;
-            this.label1.Text = "label1";
+            this.label1.Text = "NOME";
             // 
-            // dataGridView1
+            // TabelaServico
             // 
-            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.TabelaServico.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(338, 82);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(421, 275);
-            this.dataGridView1.TabIndex = 19;
+            this.TabelaServico.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.TabelaServico.Location = new System.Drawing.Point(338, 82);
+            this.TabelaServico.Name = "TabelaServico";
+            this.TabelaServico.Size = new System.Drawing.Size(421, 275);
+            this.TabelaServico.TabIndex = 19;
             // 
-            // button3
+            // AddServico
             // 
-            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button3.Location = new System.Drawing.Point(338, 363);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(127, 38);
-            this.button3.TabIndex = 18;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
+            this.AddServico.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.AddServico.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.AddServico.Location = new System.Drawing.Point(338, 363);
+            this.AddServico.Name = "AddServico";
+            this.AddServico.Size = new System.Drawing.Size(127, 38);
+            this.AddServico.TabIndex = 18;
+            this.AddServico.Text = "CADASTRAR";
+            this.AddServico.UseVisualStyleBackColor = true;
+            this.AddServico.Click += new System.EventHandler(this.AddServico_Click);
             // 
-            // button2
+            // alterarServico
             // 
-            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.Location = new System.Drawing.Point(486, 363);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(127, 38);
-            this.button2.TabIndex = 17;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.alterarServico.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.alterarServico.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.alterarServico.Location = new System.Drawing.Point(486, 363);
+            this.alterarServico.Name = "alterarServico";
+            this.alterarServico.Size = new System.Drawing.Size(127, 38);
+            this.alterarServico.TabIndex = 17;
+            this.alterarServico.Text = "ATUALIZAR";
+            this.alterarServico.UseVisualStyleBackColor = true;
+            this.alterarServico.Click += new System.EventHandler(this.alterarServico_Click);
             // 
-            // button1
+            // deletarServicos
             // 
-            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Location = new System.Drawing.Point(632, 363);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(127, 38);
-            this.button1.TabIndex = 16;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.deletarServicos.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.deletarServicos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.deletarServicos.Location = new System.Drawing.Point(632, 363);
+            this.deletarServicos.Name = "deletarServicos";
+            this.deletarServicos.Size = new System.Drawing.Size(127, 38);
+            this.deletarServicos.TabIndex = 16;
+            this.deletarServicos.Text = "DELETAR";
+            this.deletarServicos.UseVisualStyleBackColor = true;
+            this.deletarServicos.Click += new System.EventHandler(this.deletarServicos_Click);
+            // 
+            // comboServicos
+            // 
+            this.comboServicos.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboServicos.FormattingEnabled = true;
+            this.comboServicos.Location = new System.Drawing.Point(63, 134);
+            this.comboServicos.Name = "comboServicos";
+            this.comboServicos.Size = new System.Drawing.Size(192, 21);
+            this.comboServicos.TabIndex = 32;
+            this.comboServicos.SelectedIndexChanged += new System.EventHandler(this.comboServicos_SelectedIndexChanged);
             // 
             // FormServicos
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboServicos);
             this.Controls.Add(this.radioButton2);
             this.Controls.Add(this.radioButton1);
             this.Controls.Add(this.label6);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.valorBaseServico);
+            this.Controls.Add(this.servicoNome);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.TabelaServico);
+            this.Controls.Add(this.AddServico);
+            this.Controls.Add(this.alterarServico);
+            this.Controls.Add(this.deletarServicos);
             this.Name = "FormServicos";
             this.Text = "Serviços";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TabelaServico)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -232,18 +207,15 @@ namespace ModernArt.Forms
         private System.Windows.Forms.RadioButton radioButton2;
         private System.Windows.Forms.RadioButton radioButton1;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox valorBaseServico;
+        private System.Windows.Forms.TextBox servicoNome;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView TabelaServico;
+        private System.Windows.Forms.Button AddServico;
+        private System.Windows.Forms.Button alterarServico;
+        private System.Windows.Forms.Button deletarServicos;
+        private System.Windows.Forms.ComboBox comboServicos;
     }
 }
