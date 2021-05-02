899144259ced1d9c16838623a143a1d9a00e0638
peregrinno <joseluan74@gmail.com>
2021-05-02 14:56:36 -0300

    [AGORA VAI] Corrige alinhamento e padroniza fontes.

--- PaginaInicial.cs
+++ PaginaInicial.cs
@@ -203,6 +203,11 @@ namespace ModernArt
             ReleaseCapture();
             SendMessage(this.Handle, 0x112, 0xf012, 0);
         }
+
+        private void btnBotaorelatorio_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("explorer.exe", @"C:\CoralArt\Relatórios\");
+        }
         //Fim botoes de controle de janela
     }
 }
